ecb0f39ffa15f59bbc6d310cafaaca0b674d62d2
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-13 01:58:03 -0600

    initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject miraj/html "5.1.0-SNAPSHOT"
+  :description "miraj html"
+  :url "https://github.com/mobileink/miraj.html"
+  :license {:name "BSD License"
+            :url "http://polymer.github.io/LICENSE.txt"}
+  :source-paths ["src/clj"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC3"]
+                 [miraj/markup "0.1.0-SNAPSHOT"]]
+  :repl-options {:port 4001})
