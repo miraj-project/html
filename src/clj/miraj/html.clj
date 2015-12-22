7aafa5a53c766257d39c6e3024177a9c0c816676
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-22 08:25:52 -0600

    pragma tags

--- src/clj/miraj/html.clj
+++ src/clj/miraj/html.clj
@@ -414,6 +414,24 @@
     (doseq [tag html5-tags]
       (println tag))))
 
+(def html5-pragma-directives
+  "meta http-equiv pragma directives
+  http://www.w3.org/html/wg/drafts/html/master/semantics.html#pragma-directives"
+  ["http-equiv"
+   ;; {fn-name [elt-tag <validation rule>]}
+  {:content-language ["content-language" {:non-conforming "Authors are
+  encouraged to use the lang attribute instead."}]
+   :content-type ["content-type" :encoding-decl]
+   :default-style ["default-style" :string]
+   :refresh ["refresh" :refresh-syntax]
+   :set-cookie ["set-cookie" {:non-conforming "Real HTTP headers should be used instead."}]
+   ;; HTML 5.1
+   :content-security-policy ["Content-Security-Policy" :string]
+   ;; :x-ua-compatible
+   }])
+
+(make-meta-tag-fns html5-pragma-directives)
+
 (make-tag-fns nil (remove (set html5-void-elt-tags) html5-tags) nil)
 
 (make-void-elt-fns html5-void-elt-tags)
