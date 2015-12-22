1c57e2a81da68efdc69a1f618c37b71c2da7a90e
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-22 08:26:12 -0600

    proj fixes

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject miraj/html "5.1.0-SNAPSHOT"
-  :description "miraj html"
+  :description "miraj html convenience fns"
   :url "https://github.com/mobileink/miraj.html"
-  :license {:name "BSD License"
-            :url "http://polymer.github.io/LICENSE.txt"}
+  :license {:name "EPL"
+            :url "http://opensource.org/licenses/eclipse-1.0.php"}
   :source-paths ["src/clj"]
-  :dependencies [[org.clojure/clojure "1.8.0-RC3"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
                  [miraj/markup "0.1.0-SNAPSHOT"]]
   :repl-options {:port 4001})
